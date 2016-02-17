b768dd481f9f7dace18730dc5f1fce7c2a566bd8
Gamaliel Tellez <gamatellez@gmail.com>
2016-02-16 20:29:34 -0700

    start and end date from string working

--- BreakoutTemp.swift
+++ BreakoutTemp.swift
@@ -12,28 +12,43 @@ class BreakoutTemp: NSObject {
     var breakoutId:String?
     var breakoutStartTime:NSDate?
     var breakputEndtime:NSDate?
-    var breakoutDayDate:NSDate?
-    
     
     init(arrayWithInfoDictionaries:NSArray) {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "dd-MM-yyyy hh:mm"
+        var sTime = ""
+        var eTime = ""
+        
         if let dictiWithBreakoutID = arrayWithInfoDictionaries[0] as? NSDictionary {
             if let id = dictiWithBreakoutID.objectForKey("v") as? String {
                 self.breakoutId = id
             }
         }
-        if let breakoutDayDate = arrayWithInfoDictionaries[3] as? NSDictionary {
-            if let dayDate = breakoutDayDate.objectForKey("f") {
-                print(dayDate)
+       
+        if let dictionayWithBreakoutStartTime = arrayWithInfoDictionaries[1] as? NSDictionary {
+          if let stringStartTime = dictionayWithBreakoutStartTime.objectForKey("f") as? String {
+                sTime = stringStartTime
             }
         }
-        if let dictionayWithBreakoutStartTime = arrayWithInfoDictionaries[1] as? NSDictionary {
-            let stringStartTime = dictionayWithBreakoutStartTime.objectForKey("f");
-            print(stringStartTime)
-        }
-    
         if let dictionaryWithBreakoutEndTime = arrayWithInfoDictionaries[2] as? NSDictionary {
-            let stringEndTime = dictionaryWithBreakoutEndTime.objectForKey("f")
-            print(stringEndTime)
+            if let stringEndTime = dictionaryWithBreakoutEndTime.objectForKey("f") as? String {
+                eTime = stringEndTime
+            }
+        }
+        if let breakoutDayDate = arrayWithInfoDictionaries[3] as? NSDictionary {
+            if let dayDate = breakoutDayDate.objectForKey("f") as? String {
+                let fullStartTimeString = dayDate + " " + sTime
+                //.print(fullStartTimeString)
+                if let startDate = dateFormatter.dateFromString(fullStartTimeString) {
+                    self.breakoutStartTime = startDate
+                   // print(NSDateFormatter.localizedStringFromDate(startDate, dateStyle: .FullStyle, timeStyle: .FullStyle))
+                }
+                let fullEndTimeString = dayDate + " " + eTime
+                if let endDate = dateFormatter.dateFromString(fullEndTimeString) {
+                    self.breakputEndtime = endDate
+                    //print(NSDateFormatter.localizedStringFromDate(endDate, dateStyle: .FullStyle, timeStyle: .FullStyle))
+                }
+            }
         }
     }
 }
